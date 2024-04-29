807098eba869045f6a126bc5c0efdc39b1f0a96a
Micah <sep85911@outlook.com>
2024-04-29 21:55:16 +0800

    202404292155

--- 001.js
+++ 001.js
@@ -1,9 +1,25 @@
 var sFirst = "fuck you!";
-console.log(sFirst);
+// console.log(sFirst)
 var id = 1230;
 id = 2321;
 var arr = [13, 434, 545, 432];
 arr[5] = 312;
 for (var k in arr) {
-    console.log(arr[k]);
+    // console.log(arr[k])
 }
+var Cat = /** @class */ (function () {
+    function Cat(a) {
+        this.age = a;
+    }
+    Cat.prototype.getAge = function () {
+        return this.age;
+    };
+    Cat.prototype.setAge = function (nAge) {
+        this.age = nAge;
+    };
+    return Cat;
+}());
+var cat1 = new Cat(1);
+console.log(cat1.getAge());
+cat1.setAge(12);
+console.log(cat1.getAge());
--- 001.ts
+++ 001.ts
@@ -1,5 +1,5 @@
 const sFirst:string = "fuck you!"
-console.log(sFirst)
+// console.log(sFirst)
 
 let id:number=1230;
 id = 2321;
@@ -7,5 +7,33 @@ id = 2321;
 let arr = [13,434,545,432]
 arr[5] = 312
 for (const k in arr) {
-    console.log(arr[k])
+    // console.log(arr[k])
 }
+
+
+class Cat {
+    private age:number;
+    constructor(a:number) {
+
+        this.age = a
+    }
+
+    getAge()
+    {
+        return this.age;
+    }
+
+    setAge(nAge:number)
+    {
+        this.age = nAge;
+    }
+
+}
+
+let cat1 = new Cat(1);
+
+console.log(cat1.getAge())
+
+cat1.setAge(12)
+console.log(cat1.getAge())
+
