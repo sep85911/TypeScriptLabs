cf9699be1fda83dee8ffafd90113af18749d08e3
Micah <sep85911@outlook.com>
2024-05-23 14:04:43 +0800

    Ôö¼Ó002.ts ²âÊÔ

--- 002.ts
+++ 002.ts
@@ -1,3 +1,15 @@
 let arr1:[string,number] = ["abc",123]
 
 console.log(arr1[1])
+
+function asyncFunction():Promise<any>{
+
+	return new Promise<any>((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("123")
+		},2000)
+
+	})
+
+}
+
