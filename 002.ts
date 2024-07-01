1ebdf3edba4e868d4347d039ba5b129c0e80e82e
Micah <sep85911@outlook.com>
2024-07-01 21:59:06 +0800

    Map ºÍ ¶ÔÏó×ÖÃæÁ¿

--- 002.ts
+++ 002.ts
@@ -4,3 +4,18 @@ const highOrderFunction = (operation: (a: number, b: number) => number) => {
 
 console.log(highOrderFunction((a, b) => a + b)); // 输出: 8
 console.log(highOrderFunction((a, b) => a * b)); // 输出: 15
+
+
+const obj001 = {
+    name:"Micah",
+    sex:true,
+    age:39
+}
+
+const map = new Map([["abc",1],["bcd",2]])
+
+map.forEach((value,key) => {
+    console.log( key + " " + value)
+});
+
+
