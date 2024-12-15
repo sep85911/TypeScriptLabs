fbedb78a17fbfdc90107b4dccb213703469a36b3
Micah321 <15116153+micah321@user.noreply.gitee.com>
2024-12-15 10:34:22 +0800

    删除js文件 因为有了ts-node

--- 002.ts
+++ 002.ts
@@ -1,21 +1,5 @@
-const highOrderFunction = (operation: (a: number, b: number) => number) => {
-    return operation(5, 3);
-};
 
-console.log(highOrderFunction((a, b) => a + b)); // 输出: 8
-console.log(highOrderFunction((a, b) => a * b)); // 输出: 15
+let str:string = "fuck windows11"
 
 
-const obj001 = {
-    name:"Micah",
-    sex:true,
-    age:39
-}
-
-const map = new Map([["abc",1],["bcd",2]])
-
-map.forEach((value,key) => {
-    console.log( key + " " + value)
-});
-
-
+console.log(str)
