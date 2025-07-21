759644c50dde1f6eb44fccb370a670381d03fef8
Micah <sep85911@outlook.com>
2025-07-21 11:53:34 +0800

    跟着教程走的环境

--- 003.js
+++ 003.js
@@ -1,15 +1,13 @@
 let a1 = { name: "Micah", age: 39 };
 let b = a1; //a里面没有age也没有关系
-console.log(b);
 function f(x) {
     console.log(x);
 }
 // f({name:"Arrick",b:3423})   //区别于赋值，传参严格检查 多了少了都不行！ 
-const f1 = (r) => r * r * 123;
-console.log(f1(432));
-let myArr = new Array("1", "2", "3");
-console.log(myArr[2], typeof (myArr[2]));
+const f1 = (r) => r * r * 123; //箭头函数
+let myArr = new Array("1", "2", "3"); //数组对象
 const ddd = {
-    book: `fuck you!`
+    book: `Micah!`
 };
-console.log(`fuck me ${ddd.book}`); //模板字变量，必须要时反引号 ` tab上面那个按键
+console.log(`Hello ${ddd.book}`); //模板字变量，必须要时反引号 ` tab上面那个按键
+// console.log("test")
--- 003.ts
+++ 003.ts
@@ -7,9 +7,6 @@ let a1 = {name : "Micah",age : 39}
 
 let b:IPerson = a1;  //a里面没有age也没有关系
 
-console.log(b)
-
-
 function f( x:IPerson)
 {
     console.log(x)
@@ -18,20 +15,17 @@ function f( x:IPerson)
 // f({name:"Arrick",b:3423})   //区别于赋值，传参严格检查 多了少了都不行！ 
 
 
-const f1 = (r:number) => r * r * 123
-
-console.log(f1(432))
+const f1 = (r:number) => r * r * 123    //箭头函数
 
 
-let myArr:string[] = new Array("1","2","3")
-
-
-console.log(myArr[2],typeof(myArr[2]))
+let myArr:string[] = new Array("1","2","3") //数组对象
 
 
 const ddd = {
-    book:`fuck you!`
+    book:`Micah!`
 }
 
 
-console.log(`fuck me ${ddd.book}` )    //模板字变量，必须要时反引号 ` tab上面那个按键
+console.log(`Hello ${ddd.book}` )    //模板字变量，必须要时反引号 ` tab上面那个按键
+
+// console.log("test")
