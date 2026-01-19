8ca586cfcd2c70228b6f64b6470841c2453d5f6f
Micah <sep85911@outlook.com>
2026-01-19 11:07:19 +0800

    20260119更新1

---
+++ 001.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("Starting!!!!!!");
+function test(a) {
+    console.log(`年龄：${a}`); //1旁边那个键 + ${变量名}
+    console.log("fsdfsf".length);
+}
+function TestF(arg) {
+    let aaa;
+    aaa = arg;
+    return aaa;
+}
+const b = TestF("fuck you!!");
--- 001.ts
+++ 001.ts
@@ -1,3 +1,19 @@
-const sName:string = "Tang yao"
+console.log("Starting!!!!!!")
 
-console.log(sName)
+
+function test(a:number) {
+    console.log(`年龄：${a}`)   //1旁边那个键 + ${变量名}
+    console.log("fsdfsf".length)
+}
+
+
+function TestF<T>(arg:T):T{
+
+    let aaa:T;
+
+    aaa = arg;
+
+    return aaa
+}
+
+const b = TestF<string>("fuck you!!")
